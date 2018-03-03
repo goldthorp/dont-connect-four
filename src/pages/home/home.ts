9ccf93f72f5cbd70e3aeb836260a2d78064a5220
goldthorp <simon@goldthorp.com>
2018-03-03 13:48:02 -0500

    finished hard mode

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,19 +10,11 @@ import { AdMobFree, AdMobFreeBannerConfig, AdMobFreeInterstitialConfig } from '@
 export class HomePage {
 
   board: any;
-  color = 'red'
-  level: number;
+  color = 'red';
 
   constructor(public navCtrl: NavController, private storage: Storage,
               private admob: AdMobFree) {
-    storage.get('level').then( val => {
-      if(val) {
-        this.level = val;
-      } else {
-        this.level = 1;
-      }
-      this.initBoard();
-    });
+    this.initBoard();
   }
 
   ionViewDidEnter() {
@@ -35,54 +27,124 @@ export class HomePage {
     this.admob.banner.prepare().catch(e => console.log(e));
   }
 
-  // initBoard() {
-  //   this.board = new Array(6).fill(null).map(i => new Array(7).fill('empty'));
-  //   for(let i = 0; i < this.level; i++) {
-  //     this.newMove(Math.floor(Math.random()*7))
-  //   }
-  //   if(!this.checkWorkable()) {
-  //     this.initBoard();
-  //   }
-  // }
+  initBoard() {
+    this.board = new Array(6).fill(null).map(i => new Array(7).fill('empty'));
+  }
 
   newMove(cellIdx) {
     if(this.board[0][cellIdx] === 'empty') {
-      let rowIdx: number;
-      for(let i = 0; i < 6; i++) {
-        if(this.board[i][cellIdx] !== 'empty') {
-          rowIdx = i-1;
-          break;
-        } else if(i == 5) {
-          rowIdx = i;
+      let rowIdx = this.getRowIdx(cellIdx);
+      this.dropPiece(rowIdx, cellIdx, this.color).then( () => {
+        let check = this.checkFour(this.board, rowIdx, cellIdx);
+        if(check) {
+          this.clearBoard(check);
         }
-      }
-      this.board[rowIdx][cellIdx] = this.color;
-      let check = this.checkFour(rowIdx, cellIdx);
-      if(check) {
-        this.clearBoard(check);
-      }
-      // if(rowIdx == 0) {
-      //   if(this.checkFull()) {
-      //     this.nextLevel();
-      //   }
-      // }
-      this.changeColor();
+        this.continueGame();
+      });
     }
   }
 
-  changeColor() {
+  getRowIdx(cellIdx) {
+    let rowIdx: number;
+    for(let i = 0; i < 6; i++) {
+      if(this.board[i][cellIdx] !== 'empty') {
+        rowIdx = i-1;
+        break;
+      } else if(i == 5) {
+        rowIdx = i;
+      }
+    }
+    return rowIdx;
+  }
+
+  dropPiece(rowIdx, cellIdx, color) {
+    return new Promise<null>( resolve => {
+      this.board[0][cellIdx] = color;
+      if(rowIdx > 0) {
+        let i = 1;
+        let loop = () => {
+          setTimeout( () => {
+            this.board[i-1][cellIdx] = 'empty'
+            this.board[i][cellIdx] = color;
+            if(i < rowIdx) {
+              loop();
+            } else {
+              resolve();
+            }
+            i++;
+          }, 30);
+        }
+        loop();
+      } else {
+        resolve();
+      }
+    });
+
+  }
+
+  continueGame() {
     if(this.color === 'red') {
       this.color = 'yellow';
+      this.computerMove();
     } else {
       this.color = 'red';
     }
   }
 
-  checkFour(i, j) {
+
+  computerMove() {
+    let possibleMoves = this.getPossibleMoves();
+    console.log(possibleMoves);
+    for(let i = 2; i >= 0; i--) {
+      let moves = possibleMoves[i].map(i => i[1]);
+      if(moves.length != 0) {
+        let r = Math.floor(Math.random()*moves.length);
+        this.newMove(moves[r]);
+        break;
+      }
+    }
+  }
+
+  getPossibleMoves(): any {
+    let possibleMoves = { 0: [], 1: [], 2: [] };
+    for(let i = 0; i < 7; i++) {
+      if(this.board[0][i] === 'empty') {
+        switch(this.getMoveRank(i)) {
+          case 0:
+            possibleMoves[0].push([this.getRowIdx(i), i]);
+            break;
+          case 1:
+            possibleMoves[1].push([this.getRowIdx(i), i]);
+            break;
+          case 2:
+            possibleMoves[2].push([this.getRowIdx(i), i]);
+        }
+      }
+    }
+    return possibleMoves;
+  }
+
+  getMoveRank(cellIdx): number {
+    let board = new Array(6).fill(null).map((_, i) => new Array(7).fill(null).map((_, j) => this.board[i][j]));
+    let rowIdx = this.getRowIdx(cellIdx);
+    board[rowIdx][cellIdx] = 'yellow';
+    if(this.checkFour(board, rowIdx, cellIdx)) {
+      return 0;
+    }
+    board[rowIdx][cellIdx] = 'red';
+    if(this.checkFour(board, rowIdx, cellIdx)) {
+      return 1;
+    }
+    return 2;
+  }
+
+
+
+  checkFour(board, i, j) {
     let store = [];
     if(i <= 2) {
       for(let inc = 0; inc <= 3; inc++) {
-        if(this.board[i][j] === this.board[i+inc][j]) {
+        if(board[i][j] === board[i+inc][j]) {
           store.push([i+inc, j]);
           if(store.length == 4) {
             return store;
@@ -93,7 +155,7 @@ export class HomePage {
     for(let b = -3; b <= 0; b++) {
       store = [];
       for(let inc = b; inc <= b+3; inc++) {
-        if(this.board[i][j] === this.board[i][j+inc]) {
+        if(board[i][j] === board[i][j+inc]) {
           store.push([i, j+inc]);
           if(store.length == 4) {
             return store;
@@ -102,7 +164,7 @@ export class HomePage {
       }
       store = [];
       for(let inc = b; inc <= b+3; inc++) {
-        if(this.board[i+inc] && this.board[i][j] === this.board[i+inc][j+inc]) {
+        if(board[i+inc] && board[i][j] === board[i+inc][j+inc]) {
           store.push([i+inc, j+inc]);
           if(store.length == 4) {
             return store;
@@ -111,7 +173,7 @@ export class HomePage {
       }
       store = [];
       for(let inc = b; inc <= b+3; inc++) {
-        if(this.board[i-inc] && this.board[i][j] === this.board[i-inc][j+inc]) {
+        if(board[i-inc] && board[i][j] === board[i-inc][j+inc]) {
           store.push([i-inc, j+inc]);
           if(store.length == 4) {
             return store;
@@ -129,46 +191,5 @@ export class HomePage {
     }
   }
 
-  checkFull() {
-    for(let i = 0; i < 6; i++) {
-      if(this.board[i].indexOf('empty') != -1) {
-        return false;
-      }
-    }
-    return true;
-  }
-
-  nextLevel() {
-    this.level++;
-    this.storage.set('level', this.level);
-    if(this.level%2 == 1) {
-      const interstitialConfig: AdMobFreeInterstitialConfig = {
-        id: 'ca-app-pub-3456785009528609/7307796920',
-        isTesting: false,
-        autoShow: true
-      };
-      this.admob.interstitial.config(interstitialConfig);
-      this.admob.interstitial.prepare().then( () => {
-        this.initBoard();
-      }).catch(e => console.log(e));
-    } else {
-      this.initBoard();
-    }
-
-  }
-
-  checkWorkable() {
-    for(let j = 0; j < 7; j++) {
-      let above = false;
-      for(let i = 0; i < 6; i++) {
-        if(this.board[i][j] !== 'empty') {
-          above = true;
-        } else if(above) {
-          return false;
-        }
-      }
-    }
-    return true;
-  }
 
 }
